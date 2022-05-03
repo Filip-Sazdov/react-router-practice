aa8f696d80929c349852ea94e22577e90daa00c4
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-05-02 23:39:46 -0400

    add Router, Switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
