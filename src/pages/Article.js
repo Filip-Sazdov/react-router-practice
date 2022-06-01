87f7556bad3cb1abc1dc4176ed7f5ad7e39b8b71
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-06-01 13:27:10 -0400

    add redirect ability

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
 
 export default function Article() {
-  const { id } = useParams()
+  const { id } = useParams() // useParams() gives us an object of all the params used in the url we set up in the routes in App.js
   const url = `http://localhost:3000/articles/${id}`
   const { data: article, isLoading, error } = useFetch(url)
+  const history = useHistory() // this hook gives us an object with methods like .goBack() and .push(), and links we previously visited.
 
-
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 2000)
+    }
+  }, [error, history])
 
   return (
     <div>
