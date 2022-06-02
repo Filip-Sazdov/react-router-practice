bb41d949a986ae1eaed41ac23d7699e92cf4db06
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2022-06-02 16:20:18 -0400

    intro to query parameters and useLocation hook.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Contact() {
+  const queryString = useLocation().search
+
+  const queryParams = new URLSearchParams(queryString)
+
+  const name = queryParams.get('name')
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Contact {name}</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea tempora non deleniti repudiandae, voluptatem consequuntur necessitatibus odit voluptates facilis, minima veniam dolor! Repellendus quas, sequi quos pariatur est optio cumque.</p>
     </div>
   )
